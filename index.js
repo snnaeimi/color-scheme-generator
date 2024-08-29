2ab8e1493c971873b0ad788751cebef8c054b8c0
Sarina Naeimi <snnaeimi@gmail.com>
2024-08-29 02:13:53 -0700

    test

---
+++ index.js
@@ -0,0 +1,12 @@
+const testEl = document.getElementById("test");
+
+fetch("https://www.thecolorapi.com/id?hex=0047AB")
+  .then((res) => res.json())
+  .then((data) => {
+    showHexColor(data);
+  });
+
+function showHexColor(data) {
+  console.log(data.image.bare);
+  testEl.innerHTML = `<img src="${data.image.bare}">`;
+}
